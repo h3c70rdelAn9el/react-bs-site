9638d43ffe1e57e5515b74d71ce2ec5d9c25873e
Hector del Angel <hectordelangel@gmail.com>
2019-08-26 17:26:38 -0700

    Little cleaning

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,61 +2,12 @@ import React, { Component } from 'react';
 import './Navbar.css';
 import { Link, animateScroll as scroll } from "react-scroll";
 
-// export default class Nav extends Component {
-
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       menu: false
-//     };
-//     this.toggleMenu = this.toggleMenu.bind(this);
-//   }
-
-//   toggleMenu(){
-//     this.setState({ menu: !this.state.menu })
-//   }
-
-//   render() {
-
-//     const show = (this.state.menu) ? "show" : "" ;
-
-//     return (
-
-//       <nav className="navbar navbar-expand-md navbar-dark">
-//         <button className="navbar-toggler" type="button" onClick={ this.toggleMenu }>
-//           <span className="navbar-toggler-icon"></span>
-//         </button>
-//         <div className={"collapse navbar-collapse " + show}>
-//           <div className="navbar-nav">
-//             <a className="nav-item nav-link navitem" href="/">Home</a>
-//             <a className="nav-item nav-link" href="/about">About</a>
-//             <a className="nav-item nav-link" href="/skills">Skills</a>
-//             <a className="nav-item nav-link" href="/projects">Projects</a>
-//             <a className="nav-item nav-link" href="/#">Resume</a>
-//           </div>
-//         </div>
-//       </nav>
-//     );
-//   }
-// }
-
-
-
 export default class Navbar extends Component {
-
-
-
 
   state = {};
   render() {
-
-
     return (
       <nav className="navbar" id="navbar" >
-
-
-
-
         <div className="nav-content">
           <ul className="nav-items">
             <Link
@@ -70,6 +21,7 @@ export default class Navbar extends Component {
             >
               Home
             </Link>
+
             <Link
               className = "link"
 
@@ -82,6 +34,7 @@ export default class Navbar extends Component {
             >
               About
             </Link>
+
             <Link
               className = "link"
 
@@ -94,9 +47,9 @@ export default class Navbar extends Component {
             >
               Skills
             </Link>
+
             <Link
               className = "link"
-
               activeClass="active"
               to="projects"
               spy={true}
@@ -106,9 +59,9 @@ export default class Navbar extends Component {
             >
               Projects
             </Link>
+            
             <Link
               className = "link"
-
               activeClass="active"
               to="resume"
               spy={true}
