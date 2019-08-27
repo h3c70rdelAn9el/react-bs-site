92c3d037479b73455201f846f77f4653bb084eaf
Hector del Angel <hectordelangel@gmail.com>
2019-08-26 17:06:09 -0700

    Add button to resume and some styling

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -7,9 +7,12 @@ export default class Resume extends Component {
       <div className="active" id="resume">
         <div className="resume-section">
           <h1>Resume</h1>
-          <div className="resume-title">
-            <p><a href="#">Download</a></p>
+          <div className="resume-title container">
+            <a className="btn btn-primary" href="https://docs.google.com/document/d/193azpHYKOEbwjxHJ2SoAlaTqSo0Xy_UmrQAc7eW_nEk/edit?usp=sharing">
+              View
+            </a>
           </div>
+
         </div>
       </div>
     )
